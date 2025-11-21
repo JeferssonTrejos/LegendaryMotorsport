8411dc2e7cec720ffa7544b274ccc5ecacd532dd
jeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2025-11-21 12:24:14 -0600

    Característica: Se actualizó el header y se rediseño el navbar en movil

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,3 +1,4 @@
+import { MenuIcon, XIcon } from "lucide-react";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
@@ -14,92 +15,73 @@ const Header = () => {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <header className="bg-black text-white fixed w-full top-0 z-50 border-b border-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <header className="bg-custom-black-1-60 text-custom-white-3 fixed w-full top-0 z-50 border-b-2 border-custom-black-4">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          {/* Logo */}
+
           <Link to="/" className="shrink-0">
-            <div className="text-xl font-bold">
-              <span className="text-white">LEGENDARY</span>
-              <br />
-              <span className="text-red-600">MOTORSPORT</span>
+            <div className="text-xl font-bold flex flex-col ">
+              <span className="text-custom-white-1 italic -mb-1">LEGENDARY</span>
+              <span className="text-custom-red-2 italic -mt-1">MOTORSPORT</span>
             </div>
           </Link>
 
-          {/* Navigation - Desktop */}
           <nav className="hidden md:flex space-x-8">
             {navLinks.map((link) => (
               <Link
                 key={link.path}
                 to={link.path}
-                className={`text-sm font-medium transition-colors hover:text-red-600 ${
-                  isActive(link.path) ? "text-red-600" : "text-gray-300"
-                }`}
+                className={`text-sm font-medium transition-colors hover:text-red-600 ${isActive(link.path) ? "text-red-600" : "text-gray-300"
+                  }`}
               >
                 {link.name}
               </Link>
             ))}
           </nav>
 
-          {/* Mobile menu button */}
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="md:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none"
+            className="md:hidden inline-flex items-center justify-center p-2 rounded-md text-custom-white-3 hover:text-white hover:bg-custom-black-3"
           >
             <span className="sr-only">Abrir menú</span>
-            {!isMenuOpen ? (
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            )}
+            <MenuIcon className="block h-6 w-6" />
           </button>
         </div>
       </div>
 
-      {/* Mobile menu */}
       {isMenuOpen && (
         <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-gray-900">
-            {navLinks.map((link) => (
-              <Link
-                key={link.path}
-                to={link.path}
-                onClick={() => setIsMenuOpen(false)}
-                className={`block px-3 py-2 rounded-md text-base font-medium ${
-                  isActive(link.path)
-                    ? "text-red-600 bg-gray-800"
-                    : "text-gray-300 hover:text-white hover:bg-gray-800"
-                }`}
-              >
-                {link.name}
+          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-custom-black-2 px-6 sm:max-w-sm ">
+            <div className="h-16 flex justify-between items-center border-b-2 border-custom-black-4">
+              <Link to="/" className="shrink-0">
+                <div className="text-xl font-bold flex flex-col ">
+                  <span className="text-custom-white-1 italic -mb-1">LEGENDARY</span>
+                  <span className="text-custom-red-2 italic -mt-1">MOTORSPORT</span>
+                </div>
               </Link>
-            ))}
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="md:hidden inline-flex items-center justify-center p-2 rounded-md text-custom-white-3 hover:text-white hover:bg-custom-black-3"
+              >
+                <span className="sr-only">Abrir menú</span>
+                <XIcon className="block h-6 w-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root space-y-2">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.path}
+                  to={link.path}
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`block px-3 py-3 rounded-md text-base font-medium ${isActive(link.path)
+                    ? "text-custom-red-1 bg-custom-black-3"
+                    : "text-custom-white-3 hover:text-custom-red-1"
+                    }`}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       )}
